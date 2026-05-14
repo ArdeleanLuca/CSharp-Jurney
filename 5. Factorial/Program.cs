cb8a84f01aa8344cfda9947d89fb2f7ea1919bc9
Ardelean Luca <ardeleanluca610@gmail.com>
2026-05-14 02:22:46 -0700

    Update Program.cs

--- 5. Factorial/Program.cs
+++ 5. Factorial/Program.cs
@@ -4,9 +4,9 @@ public class HelloWorld
 {
     public static void Main(string[] args)
     {
-        //factorial
-        int factorial = 1;
+        Console.Write("Nr: ");
         int number = int.Parse(Console.ReadLine());
+        int factorial = 1;
         for (int i = 1; i <= number; i++)
         {
             factorial *= i;
